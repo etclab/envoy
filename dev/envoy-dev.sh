c46eea1828feecdeb6b350cfb449a2645d40e2d3
Aashutosh Poudel <apoudel01@wm.edu>
2025-01-16 12:51:00 -0500

    only release builds get picked up by docker builds

--- dev/envoy-dev.sh
+++ dev/envoy-dev.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-./ci/run_envoy_docker.sh './ci/do_ci.sh debug.server_only'
+# set -Eeuo pipefail
+# set -x
+
+./ci/run_envoy_docker.sh './ci/do_ci.sh release.server_only'
 
 ENVOY_DOCKER_IN_DOCKER=1 ./ci/run_envoy_docker.sh './ci/do_ci.sh docker'
